a48d906a76e39cdf41ecb6b8b56309bf22023b6d
Nathan <88581278+nathannael71@users.noreply.github.com>
2025-05-14 19:42:47 +0700

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,6 +2,6 @@ import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind';
 // Combines multiple class names or class name objects into a single string
 // Uses tailwind-merge to handle conflicting tailwind classes
-export function cn(…inputs: ClassValue[]): string {
-return twMerge(clsx(inputs));
+export function cn(...inputs: ClassValue[]): string {
+  return twMerge(clsx(inputs));
 }
