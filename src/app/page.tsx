0bceb7e0e29ed7f7ef73944dacb95d9022c9e3b1
Nathan <88581278+nathannael71@users.noreply.github.com>
2025-05-17 21:47:03 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,1 @@
-// src/app/page.tsx
-import Header from '@/components/layout/Header';
 
-export default function Home() {
-  return (
-    <main className="pt-[44px]">
-      <Header />
-    </main>
-  );
-}
